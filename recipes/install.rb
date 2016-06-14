8d3cf74c5d1bd845979ac30d2f3dacfc43010dc6
%github_username% <jdowling@sics.se>
2016-06-14 14:49:44 +0200

    log4j fix

--- recipes/install.rb
+++ recipes/install.rb
@@ -29,40 +29,6 @@ when "redhat"
  include_recipe "scala"
 end
 
-
-# package_url = node.drelephant.play_url
-# base_package_filename =  File.basename(package_url)
-# base_package_zipname =  File.basename(base_package_filename, ".zip")
-
-# remote_file "#{Chef::Config.file_cache_path}/#{base_package_filename}" do
-#   checksum node.drelephant.play_checksum
-#   source package_url
-#   owner node.drelephant.user
-#   group node.drelephant.group
-#   mode 0755
-#   action :create
-# end
-
-# playlock = "#{Chef::Config.file_cache_path}/.play_downloaded"
-
-# bash "unpack_typesafe_play" do
-#     user "root"
-#     code <<-EOF
-#     set -e
-#     if [ -L #{node.drelephant.play_base_dir}  ; then
-#        rm -rf #{node.drelephant.play_base_dir} 
-#     fi
-#     cd #{Chef::Config.file_cache_path}
-#     unzip #{Chef::Config.file_cache_path}/#{base_package_filename}
-#     mv #{base_package_zipname} #{node.drelephant.dir}
-#     ln -s #{node.drelephant.play_home} #{node.drelephant.play_base_dir}
-#     chown -R #{node.drelephant.user} #{node.drelephant.play_home}
-#     touch #{playlock}
-# EOF
-#   not_if { ::File.exists?( playlock ) }
-# end
-
-
 dr_url = node.drelephant.url
 basename =  File.basename(dr_url)
 base_zipname =  File.basename(basename, ".zip")
