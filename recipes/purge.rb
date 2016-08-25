71ad1e9a5241b6972aab873b08fc1ffcafb909cc
Jim Dowling <jdowling@sics.se>
2016-08-25 22:08:49 +0200

    Purge recipe added

---
+++ recipes/purge.rb
@@ -0,0 +1,38 @@
+  bash 'kill_running_service_drelephant' do
+    user "root"
+    ignore_failure true
+    code <<-EOF
+      service stop drelephant
+      systemctl stop drelephant
+      pkill -9 drelephant
+    EOF
+  end
+
+  file "/etc/init.d/drelephant" do
+    action :delete
+    ignore_failure true
+  end
+  
+  file "/usr/lib/systemd/system/drelephant.service" do
+    action :delete
+    ignore_failure true
+  end
+  file "/lib/systemd/system/drelephant.service" do
+    action :delete
+    ignore_failure true
+  end
+
+
+
+  directory node.drelephant.home do
+    recursive true
+    action :delete
+    ignore_failure true
+  end
+
+  link node.drelephant.base_dir do
+    action :delete
+    ignore_failure true
+  end
+
+
