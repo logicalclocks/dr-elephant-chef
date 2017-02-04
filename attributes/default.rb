722e46d351b303a9c62f36803dd99bb58929ba8e
Theofilos Kakantousis <t.kakantousis@gmail.com>
2017-02-04 22:25:33 +0100

    upgrade dr-elephant version

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,7 +4,7 @@ include_attribute "apache_hadoop"
 
 node.default.drelephant.user                = "glassfish"
 node.default.drelephant.group               = node.apache_hadoop.group
-node.default.drelephant.version             = "2.0.3-SNAPSHOT"
+node.default.drelephant.version             = "2.0.6"
 node.default.drelephant.url                 = "http://snurran.sics.se/hops/dr-elephant-#{node.drelephant.version}.zip"
 node.default.drelephant.checksum            = ""
 node.default.drelephant.port                = "11011"
@@ -13,7 +13,7 @@ node.default.drelephant.dir                 = "/srv"
 node.default.drelephant.base_dir            = node.drelephant.dir + "/dr-elephant"
 node.default.drelephant.home                = node.drelephant.dir + "/dr-elephant-" + node.drelephant.version
 
-node.default.drelephant.play_version        = "1.3.10"
+node.default.drelephant.play_version        = "1.3.12"
 node.default.drelephant.play_url            = "https://downloads.typesafe.com/typesafe-activator/#{node.drelephant.play_version}/typesafe-activator-#{node.drelephant.play_version}-minimal.zip"
 node.default.drelephant.play_checksum       = "15352ce253aa804f707ef8be86390ee1ee91da4b78dbb2729ab1e9cae01d8937"
 node.default.drelephant.play_base_dir       = node.drelephant.dir + "/activator-dist"
