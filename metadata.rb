7d629533615e25eb12ae058e8bd83bbfc23b31a9
Jim Dowling <jdowling@sics.se>
2017-02-28 16:06:05 -0800

    Revert "removing apache_hadoop dependency"
    This reverts commit 6203ad46711f2330e6626b6bd1ed7293a7fdea07.

--- metadata.rb
+++ metadata.rb
@@ -14,6 +14,7 @@ depends 'kagent'
 #depends 'scala'
 depends 'java'
 depends 'hadoop_spark'
+depends 'apache_hadoop'
 
 recipe  "drelephant::install", "Installs the binaries for Dr Elephant"
 recipe  "drelephant::default", "Starts Dr Elephant server."
